31d7b3d5777bbfd3c0f98aec7334f54d8972aa8c
Elisabeth FAUJOUR <efaujour@gmail.com>
2024-03-25 18:02:03 +0100

    :memo: Replace Comments on App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,10 +4,6 @@ import Header from '../Header/Header';
 import './App.scss';
 
 function App() {
-  // 2. Attention, si drapeau dans la Cell on ne peux pas clique gauche dessus
-
-  // 3. Fix bug: lorsque je pose le dernier drapeau + toutes les cells no bombs revealed ça ne termine pas la game
-
   return (
     <div className="app">
       <Header />
